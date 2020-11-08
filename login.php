85e4beab2e3f8af09a6a0bb036f10f16f8ea86b8
Yvon Huynh <yvon.huynh@gmail.com>
2020-11-08 21:31:26 +0100

    gestion password vide

--- login.php
+++ login.php
@@ -6,12 +6,12 @@
 // si n'existe pas je redirige vers error.php avec un message d'erreur (contient un lien pour rediriger vers index.php)
 // si l'utilisateur existe on va définir une session authentifiée (l'utilisateur est loggé))
 // si connecté propose le logout
-print_r($_POST);
-
 if(count($_POST) > 0){
   if($_POST['email'] == 'yvon@gmail.com' && $_POST['password'] == '123'){
     $_SESSION['email'] = 'yvon@gmail.com';
     header('Location: http://sandbox.test/liste.php');
+  } else {
+    header('Location: http://sandbox.test/login.php');
   }
 
 } else {
@@ -29,11 +29,11 @@ if(count($_POST) > 0){
     <form action="login.php" method="post">
     <div class="form-group">
       <label for="exampleInputEmail1">Email</label>
-      <input type="email" class="form-control" name="email" id="exampleInputEmail1" value="yvon@gmail.com" aria-describedby="emailHelp">
+      <input required type="email" class="form-control" name="email" id="exampleInputEmail1" value="yvon@gmail.com" aria-describedby="emailHelp">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+      <input required type="password" name="password" class="form-control" id="exampleInputPassword1">
     </div>
   <button type="submit" class="btn btn-primary">Soumettre</button>
 </form>
