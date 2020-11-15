7151d331489ebb5f8a4198e0d140786cc5a8f062
Yvon Huynh <yvon.huynh@gmail.com>
2020-11-15 12:10:13 +0100

    change styling

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
 <?php
   include('includes/header.php');
   ?>
-    <body>
-    <h1>Les Formulaires</h1>
+<body>
+
 <div class="container">
-
-    <form method="post" action="login.php">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" name="email" value="yvon@gmail.com">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="password" value="123456">
-  </div>
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<h1>Bienvenue sur DiscoApp</h1>
+<p>DiscoApp est une mini application démontrant les possibilité de PHP</p>
 </div>
 
 
 
 
 
-  </body>
+  /body>
 
   <?php
   include('includes/snippets.php');
