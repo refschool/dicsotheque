bd36b9b7aeb9f72823d309bbc5664b30e9f503f9
Yvon Huynh <yvon.huynh@gmail.com>
2020-11-15 12:37:30 +0100

    form OK , hash OK

--- user-add.php
+++ user-add.php
@@ -2,41 +2,56 @@
   include('includes/header.php');
   include('includes/function-pdo.php');
 
-// je vérifie que email et password ne soient pas vide
-// je vais chercher dans la table utilisateur que l'utilisateur existe
-// si n'existe pas je redirige vers error.php avec un message d'erreur (contient un lien pour rediriger vers index.php)
-// si l'utilisateur existe on va définir une session authentifiée (l'utilisateur est loggé))
-// si connecté propose le logout
-if(count($_POST) > 0){
-  if(isValid($_POST['email'] ,$_POST['password'],$pdo) ){
-    $_SESSION['email'] = $_POST['email'];
-    header('Location: http://sandbox.test/liste.php');
-  } else {
+  if(!isset($_SESSION['email'])){
     header('Location: http://sandbox.test/login.php');
   }
 
-} else {
 
-  ?>
+
+?>
 
   <body>
   <?php
   include('includes/navbar.php');
+
+  
+// je vérifie que email et password ne soient pas vide
+// je vérifie que l'utilisateur n'existe pas déjà
+// je chiffre le password avec bcrypt
+
+if(isset($_POST['password']) ){
+    $crypted_password = password_hash($_POST['password'], PASSWORD_BCRYPT );
+    $email = $_POST['email'];
+    echo $crypted_password;
+
+}
+
+//insertion dans la base de donnée
+function addUser($pdo){
+
+    $sql = "";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   
+    return $result;
+  }
   ?>
-    <h1>Page de login</h1>
+
+
 
 <div class="container">
-    
-    <form action="login.php" method="post">
+        <h1>Création d'un utilisateur</h1>
+    <form action="user-add.php" method="post">
     <div class="form-group">
       <label for="exampleInputEmail1">Email</label>
-      <input required type="email" class="form-control" name="email" id="exampleInputEmail1" value="yvon@gmail.com" aria-describedby="emailHelp">
+      <input required type="email" class="form-control" name="email" id="exampleInputEmail1" value="toto@gmail.com" aria-describedby="emailHelp">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
       <input required type="password" name="password" class="form-control" id="exampleInputPassword1">
     </div>
-  <button type="submit" class="btn btn-primary">Soumettre</button>
+  <button type="submit" class="btn btn-primary">Créer un utilisateur</button>
 </form>
 </div>
 
@@ -45,7 +60,3 @@ if(count($_POST) > 0){
 include('includes/snippets.php');
 ?>
 </html>
-
-<?php
-}
-?>
