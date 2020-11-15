e933d20dd343816fa135bf8a6cd17d5e2f8c7eb8
Yvon Huynh <yvon.huynh@gmail.com>
2020-11-15 12:15:34 +0100

    add file

--- detail.php
+++ detail.php
@@ -3,6 +3,16 @@
   include('includes/header.php');
 
   print_r($_SESSION);
+
+  $idalbum = 0;
+
+  if(isset($_GET['idalbum'])){
+    $idalbum = $_GET['idalbum'];
+
+
+    // TODO : fonction pour chercher les informations de l'album
+
+  }
   
   if(!isset($_SESSION['email'])){
     header('Location: http://sandbox.test/login.php');
---
+++ user-add.php
@@ -0,0 +1,51 @@
+<?php
+  include('includes/header.php');
+  include('includes/function-pdo.php');
+
+// je vérifie que email et password ne soient pas vide
+// je vais chercher dans la table utilisateur que l'utilisateur existe
+// si n'existe pas je redirige vers error.php avec un message d'erreur (contient un lien pour rediriger vers index.php)
+// si l'utilisateur existe on va définir une session authentifiée (l'utilisateur est loggé))
+// si connecté propose le logout
+if(count($_POST) > 0){
+  if(isValid($_POST['email'] ,$_POST['password'],$pdo) ){
+    $_SESSION['email'] = $_POST['email'];
+    header('Location: http://sandbox.test/liste.php');
+  } else {
+    header('Location: http://sandbox.test/login.php');
+  }
+
+} else {
+
+  ?>
+
+  <body>
+  <?php
+  include('includes/navbar.php');
+  ?>
+    <h1>Page de login</h1>
+
+<div class="container">
+    
+    <form action="login.php" method="post">
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email</label>
+      <input required type="email" class="form-control" name="email" id="exampleInputEmail1" value="yvon@gmail.com" aria-describedby="emailHelp">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input required type="password" name="password" class="form-control" id="exampleInputPassword1">
+    </div>
+  <button type="submit" class="btn btn-primary">Soumettre</button>
+</form>
+</div>
+
+</body>
+<?php
+include('includes/snippets.php');
+?>
+</html>
+
+<?php
+}
+?>
