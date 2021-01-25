90be27d8dc6a21f9b0b8629d70d02b8f3ca95dd0
Yvon Huynh <yvon.huynh@gmail.com>
2021-01-25 23:16:40 +0100

    ras

--- includes/function-pdo.php
+++ includes/function-pdo.php
@@ -1,20 +1,20 @@
 <?php
 
-function isValid($email,$password,$pdo){
-    
+function isValid($email, $password, $pdo)
+{
+
 
     $sql = "SELECT email,password FROM utilisateurs WHERE email = :email";
     $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':email',$email);
+    $stmt->bindParam(':email', $email);
     $stmt->execute();
     $result = $stmt->fetchAll();
 
-    if(count($result) > 0){
-        if($result[0]['password'] == $password){
+    if (count($result) > 0) {
+        if ($result[0]['password'] == $password) {
             return true;
         }
     }
 
     return false;
 }
-
